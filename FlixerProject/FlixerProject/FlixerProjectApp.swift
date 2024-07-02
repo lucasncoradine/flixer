629f1c3254774c84990bc7e0c2e0de1290a02439
Lucas Coradine <lucasncoradine@gmail.com>
2024-07-02 10:18:50 -0300

    initial commit

---
+++ FlixerProject/FlixerProject/FlixerProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlixerProjectApp.swift
+//  FlixerProject
+//
+//  Created by Lucas Coradine on 02/07/24.
+//
+
+import SwiftUI
+
+@main
+struct FlixerProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
