43c990ab09e75768427d8522cbfc920ecb2f80cf
Lucas Coradine <lucasncoradine@gmail.com>
2024-07-18 14:33:00 -0300

    Restoring file;Documenting extension

--- FlixerProject/FlixerProject/ContentView.swift
+++ FlixerProject/FlixerProject/ContentView.swift
@@ -1,16 +1,12 @@
 import SwiftUI
 
 struct ContentView: View {
-    let imageUrl = "https://picsum.photos/id/12/200"
-
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
-
-            Avatar(url: imageUrl, size: .large)
         }
         .padding()
     }
--- FlixerProject/FlixerProject/Helpers/String+Extension.swift
+++ FlixerProject/FlixerProject/Helpers/String+Extension.swift
@@ -1,4 +1,5 @@
 extension String {
+    /// Checks if the string is not empty
     var isNotEmpty: Bool {
         self.isEmpty == false
     }
