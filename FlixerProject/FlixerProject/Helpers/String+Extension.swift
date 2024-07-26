1f58c77f3ab6231303cb9aa9541c110ba00cafd5
Lucas Coradine <lucasncoradine@gmail.com>
2024-07-26 16:00:02 -0300

    Updating Icon images and component

--- FlixerProject/FlixerProject/Helpers/String+Extension.swift
+++ FlixerProject/FlixerProject/Helpers/String+Extension.swift
@@ -3,4 +3,20 @@ extension String {
     var isNotEmpty: Bool {
         self.isEmpty == false
     }
+
+    /// Turns a Camel-Cased string into Kebab Case
+    func toKebabCase() -> String {
+        var result  = self.replacingOccurrences(of: "([A-Z])",
+                                                with: "-$1",
+                                                options: .regularExpression,
+                                                range: range(of: self))
+            .trimmingCharacters(in: .whitespacesAndNewlines)
+            .lowercased()
+
+        if result.first == "-" {
+            result.removeFirst()
+        }
+
+        return result
+    }
 }
